706c84c86b6962d0d9ff3f042149a423d6f8665b
shhr3y <shhr3y@github.com>
2020-04-25 21:43:42 +0530

    same but faster using using splitting

---
+++ rasPi/stream_client_fast.py
@@ -0,0 +1,47 @@
+import io
+import socket
+import struct
+import time
+import picamera
+import sys
+
+class SplitFrames(object):
+    def __init__(self, connection):
+        self.connection = connection
+        self.stream = io.BytesIO()
+        self.count = 0
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # Start of new frame; send the old one's length
+            # then the data
+            size = self.stream.tell()
+            if size > 0:
+                self.connection.write(struct.pack('<L', size))
+                self.connection.flush()
+                self.stream.seek(0)
+                self.connection.write(self.stream.read(size))
+                self.count += 1
+                self.stream.seek(0)
+        self.stream.write(buf)
+
+my_server = '192.168.0.107'
+res = (320, 240)
+client_socket = socket.socket()
+client_socket.connect((my_server, 8000))
+connection = client_socket.makefile('wb')
+try:
+    output = SplitFrames(connection)
+    with picamera.PiCamera(resolution=res, framerate=30) as camera:
+        time.sleep(2)
+        start = time.time()
+        camera.start_recording(output, format='mjpeg')
+        camera.wait_recording(sys.maxint)
+        camera.stop_recording()
+        connection.write(struct.pack('<L', 0))
+finally:
+    finish = time.time()
+    print('Sent %d images in %d seconds at %.2ffps' % (
+    output.count, finish-start, output.count / (finish-start)))
+    connection.close()
+    client_socket.close()
