01d27ed81162ce17be5b49b6719a9ce183a409c2
shhr3y <shhr3y@github.com>
2020-04-25 21:26:56 +0530

    Control RC Car from Mac Keyboard using pygame
    for testing puroose

---
+++ testing/rc_control_test.py
@@ -0,0 +1,69 @@
+import serial
+import pygame
+from pygame.locals import *
+
+
+class RCTest(object):
+
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_mode((250, 250))
+        self.ser = serial.Serial("/dev/cu.usbmodem141401", 115200, timeout=1)
+        self.send_inst = True
+        self.steer()
+
+    def steer(self):
+
+        while self.send_inst:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    key_input = pygame.key.get_pressed()
+
+                    # dual orders
+                    if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
+                        print("Forward Right")
+                        self.ser.write(chr(6).encode())
+
+                    elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
+                        print("Forward Left")
+                        self.ser.write(chr(7).encode())
+
+                    elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
+                        print("Reverse Right")
+                        self.ser.write(chr(8).encode())
+
+                    elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
+                        print("Reverse Left")
+                        self.ser.write(chr(9).encode())
+
+                    # single orders
+                    elif key_input[pygame.K_UP]:
+                        print("Forward")
+                        self.ser.write(chr(1).encode())
+
+                    elif key_input[pygame.K_DOWN]:
+                        print("Reverse")
+                        self.ser.write(chr(2).encode())
+
+                    elif key_input[pygame.K_RIGHT]:
+                        print("Right")
+                        self.ser.write(chr(3).encode())
+
+                    elif key_input[pygame.K_LEFT]:
+                        print("Left")
+                        self.ser.write(chr(4).encode())
+
+                    # exit
+                    elif key_input[pygame.K_x] or key_input[pygame.K_q]:
+                        print("Exit")
+                        self.send_inst = False
+                        self.ser.write(chr(0).encode())
+                        self.ser.close()
+                        break
+
+                elif event.type == pygame.KEYUP:
+                    self.ser.write(chr(0).encode())
+
+
+if __name__ == '__main__':
+    RCTest()
